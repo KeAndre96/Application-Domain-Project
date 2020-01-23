dfafbbccf922dbdd6765edbbd0e37cbc9c4bf052
Henry Homiller <henryhom97@gmail.com>
2020-01-23 16:44:20 -0500

    Admin can now toggle the active state of accounts

--- Pages/Admin/Accounts.cshtml.cs
+++ Pages/Admin/Accounts.cshtml.cs
@@ -23,7 +23,33 @@ namespace AppDomainProject.Pages.Admin
         public void OnGet()
         {
             var query = from u in _context.UserInfoData select u;
+            
             Users = query.ToList();
+        }
+
+        public IActionResult OnPostAcctToggle(string id, int status)
+        {
+            var query = from u in _context.UserInfoData select u;
+            query = query.Where(m => m.ID.Equals(id));
+            UserInfoData data = query.FirstOrDefault();
+            if (data != null)
+            { 
+                data.Status = (AccountStatus)status;
+                _context.Entry(data).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _context.SaveChanges();
+            }
+
+            return Redirect("./Accounts");
+        }
+
+        public int GetToggleStatus(UserInfoData u)
+        {
+            return u.Status == AccountStatus.Active ? (int)AccountStatus.Inactive : (int)AccountStatus.Active;
+        }
+
+        public string GetUpdateText(UserInfoData u)
+        {
+            return u.Status == AccountStatus.Active ? "Deactivate" : "Activate";
         }
     }
 }
