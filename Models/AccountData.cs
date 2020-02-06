46fac581569011f940a5368eee55c804f964751c
KeAndre96 <grahamkeandre@gmail.com>
2020-02-05 19:29:08 -0500

    Added model page for accounts

---
+++ Models/AccountData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppDomainProject.Models
+{
+    public class AccountData
+    {
+        [Key]
+        public string AccountNumber { get; set; }
+
+        public string AccountName { get; set; }
+
+        public string AccountDescription { get; set; }
+
+        public string NormalSide { get; set; }
+
+        public string AccountCategory { get; set; }
+
+        public string AccountSubCategory { get; set; }
+
+    }
+}
--- Pages/ShowAccountsPending.cshtml.cs
+++ Pages/ShowAccountsPending.cshtml.cs
@@ -26,11 +26,11 @@ namespace AppDomainProject
 
         }
         
-        public async void OnPostActivate(string id)
+        public async Task<IActionResult> OnPostActivate(string id)
         {
             if(id == null)
             {
-                NotFound();
+                return NotFound();
             }
             var UserInfoData = await _context.UserInfoData.ToListAsync();
             var tempUser = UserInfoData.Find(m => m.ID.Equals(id));
@@ -45,14 +45,14 @@ namespace AppDomainProject
             {
                 if (!UserInfoDataExists(tempUser.ID))
                 {
-                    NotFound();
+                    return NotFound();
                 }
                 else
                 {
                     throw;
                 }
             }
-            RedirectToPage("./");
+           return  RedirectToPage("./");
         }
         
         public async Task<IActionResult> OnPostDelete(string id)
@@ -70,7 +70,7 @@ namespace AppDomainProject
                 _context.UserInfoData.Remove(tempUser);
                 await _context.SaveChangesAsync();
             }
-            return RedirectToPage("./");
+            return RedirectToPage("./ShowAccountsPending");
         }
         
         private bool UserInfoDataExists(string id)
