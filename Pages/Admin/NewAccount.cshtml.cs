ee419edf962aab5b6e8c439bb879caa2b1cb9ff4
Henry Homiller <henryhom97@gmail.com>
2020-02-14 11:00:48 -0500

    Fixed up the new account page

--- Pages/Admin/NewAccount.cshtml.cs
+++ Pages/Admin/NewAccount.cshtml.cs
@@ -17,6 +17,10 @@ namespace AppDomainProject
         [BindProperty]
         public AccountData Account { get; set; }
 
+        [BindProperty]
+        public string Side { get; set; }
+        public string[] Sides = new string[] { "Credit", "Debit" };
+
         public void OnGet()
         {
 
@@ -30,6 +34,8 @@ namespace AppDomainProject
             Account.Credit = 0;
             Account.Debit = 0;
             Account.Balance = Account.InitialBalance;
+
+            Account.NormalSide = Side.Equals(Sides[0]);
 
             _context.Attach(Account).State = Microsoft.EntityFrameworkCore.EntityState.Added;
             _context.SaveChanges();
