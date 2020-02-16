3d23f175e34681dbab2aa007a59da149f3ea0650
KeAndre96 <grahamkeandre@gmail.com>
2020-02-16 13:53:01 -0500

    Added search bar to Accounts.cshtml page.

--- Models/PersonalInfoData.cs
+++ Models/PersonalInfoData.cs
@@ -9,16 +9,11 @@ namespace AppDomainProject.Models
     public class PersonalInfoData
     {
         [Key]
-        [Required]
         public string ID { get; set; }
-        [Required]
         public string FirstName { get; set; }
-        [Required]
         public string LastName { get; set; }
-        [Required]
         [DataType(DataType.Date)]
         public DateTime DOB { get; set; }
-        [Required]
         public string Address { get; set; }
     }
 }
