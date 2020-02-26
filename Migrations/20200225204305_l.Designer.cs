00f2d4bba0f9d5cd7a08641775759b23fa1762b3
KeAndre96 <grahamkeandre@gmail.com>
2020-02-26 18:55:47 -0500

    Added EventLog for Admin

--- Migrations/20200225204305_l.Designer.cs
+++ Migrations/20200225204305_l.Designer.cs
@@ -4,14 +4,16 @@ using AppDomainProject.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AppDomainProject.Migrations
 {
     [DbContext(typeof(AppDomainProjectContext))]
-    partial class AppDomainProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20200225204305_l")]
+    partial class l
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Pages/Admin/EditAccount.cshtml.cs
+++ Pages/Admin/EditAccount.cshtml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AppDomainProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace AppDomainProject
 {
@@ -33,11 +36,47 @@ namespace AppDomainProject
 
         public IActionResult OnPost()
         {
-            if(!string.IsNullOrEmpty(Side))
+            if (!string.IsNullOrEmpty(Side))
+            {
                 Account.NormalSide = Side.Equals(Sides[0]);
+                //sb.Append();
+                //EventLogData temp = new EventLogData { id=}
+            }
 
             _context.Attach(Account).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
             _context.SaveChanges();
+            string columnName = "";
+            foreach(EntityEntry e in _context.ChangeTracker.Entries())
+            {
+                if(e.State.Equals("Modified"))
+                {
+                    columnName = e.Entity.ToString();
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            //string id = UserInfo.ID;
+            Random r = new Random();
+            string id = r.Next().ToString();
+            var ans = _context.EventLogData.Find(id);
+            if(ans == null)
+            {
+                sb.Append(UserInfo.ID + " changed something in Chart of Accounts");
+                EventLogData temp = new EventLogData { id = id, log = sb.ToString() };
+                _context.EventLogData.Add(temp);
+            }
+            else
+            {
+                while(ans != null)
+                {
+                    id = r.Next().ToString();
+                    ans = _context.EventLogData.Find(id);
+                }
+                sb.Append(UserInfo.ID + " changed something in Chart of Accounts");
+                EventLogData temp = new EventLogData { id = id, log = sb.ToString() };
+                _context.EventLogData.Add(temp);
+            }
+            _context.SaveChanges();
+
             return Redirect($"/User/Account/{Account.AccountNumber}");
         }
 
