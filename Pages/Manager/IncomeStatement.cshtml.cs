b8a7d6cd3dbfb328cb76385349f94ac9fac11777
KeAndre96 <grahamkeandre@gmail.com>
2020-04-08 15:13:30 -0400

    Added data for retain earnings and income statement

--- Pages/Manager/IncomeStatement.cshtml.cs
+++ Pages/Manager/IncomeStatement.cshtml.cs
@@ -1,17 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using AppDomainProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AppDomainProject
 {
-    public class IncomeStatementModel : PageModel
+    public class IncomeStatementModel : ManagerPageModel
     {
-        public void OnGet()
-        {
+        public IncomeStatementModel(AppDomainProjectContext _context) : base(_context) { }
 
+        [BindProperty]
+        public double revenue_total { get; set; }
+
+        [BindProperty]
+        public double expenses_total { get; set; }
+
+        [BindProperty]
+        public double net_total { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [BindProperty]
+        public List<AccountData> Revenue { get; set; }
+
+        [BindProperty]
+        public List<AccountData> Expenses { get; set; }
+        public void OnGet(DateTime? date)
+        {
+            Date = date.HasValue ? date.Value : DateTime.Now;
+
+            revenue_total = 0;
+            expenses_total = 0;
+            net_total = 0;
+
+            Revenue = new List<AccountData>();
+            Expenses = new List<AccountData>();
+
+            var q = from AccountData a in _context.AccountData where a.TimeAccountAdded < Date select a;
+
+            foreach(AccountData a in q)
+            {
+                if(a.Statement == 0)
+                {
+                    if(a.AccountCategory.Equals("Asset"))
+                    {
+                        Revenue.Add(a);
+                        revenue_total += a.Balance;
+                    }
+                    else
+                    {
+                        Expenses.Add(a);
+                        expenses_total += a.Balance;
+                    }
+                }
+            }
+            net_total = revenue_total - expenses_total;
         }
+        public IActionResult OnPost()
+        {
+            return Redirect($"./IncomeStatement?date={Date}");
+        }
+
     }
 }
--- Pages/Manager/Retaining_Earnings_Statement.cshtml.cs
+++ Pages/Manager/Retaining_Earnings_Statement.cshtml.cs
@@ -4,14 +4,63 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using AppDomainProject.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace AppDomainProject
 {
-    public class Retaining_Earnings_StatementModel : PageModel
+    public class Retaining_Earnings_StatementModel : ManagerPageModel
     {
-        public void OnGet()
-        {
+        public Retaining_Earnings_StatementModel(AppDomainProjectContext _context) : base(_context) { }
 
+        [BindProperty]
+        public List<string> temp { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [BindProperty]
+        public double[] array { get; set; }
+
+        public void OnGet(DateTime? date)
+        {
+            Date = date.HasValue ? date.Value : DateTime.Now;
+
+            temp = new List<string>();
+            temp.Add("Beginning Balence");
+            temp.Add("Net Income");
+            temp.Add("Less Drawings");
+            temp.Add("Ending Balence");
+
+            double revenue_total = 0;
+            double expenses_total = 0;
+            double net_total = 0;
+
+
+            var q = from AccountData a in _context.AccountData where a.TimeAccountAdded < Date select a;
+
+            foreach (AccountData a in q)
+            {
+                if (a.Statement == 0)
+                {
+                    if (a.AccountCategory.Equals("Asset"))
+                    {
+                        revenue_total += a.Balance;
+                    }
+                    else
+                    {
+                        expenses_total += a.Balance;
+                    }
+                }
+            }
+            net_total = revenue_total - expenses_total;
+            array = new double[4];
+            array[1] = net_total;
+        }
+        public IActionResult OnPost()
+        {
+            return Redirect($"./Retaining_Earnings_Statement?date={Date}");
         }
     }
 }
